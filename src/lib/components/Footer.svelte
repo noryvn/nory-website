0e95eb85fd0c5a49e063a3b31776ab26f09e2fed
falentio <riv.kevinfalent.io@gmail.com>
2022-11-06 22:38:57 +0700

    chore: something

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,5 +1,5 @@
 <div class="footer text-md p-2 border-base-content border-t">
 	<div class="container mx-auto">
-		<p>Di Buat Dengan &hearts; oleh Kevin Falentio &copy; 2022</p>
+		<p>Di Buat oleh Kevin Falentio &copy; 2022</p>
 	</div>
 </div>
