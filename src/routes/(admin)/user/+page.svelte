d4cf75b7603bb4e926928afbeef63a3bd4e9aca3
falentio <riv.kevinfalent.io@gmail.com>
2022-11-11 18:59:02 +0700

    feat: class info using name

---
+++ src/routes/(admin)/user/+page.svelte
@@ -0,0 +1,79 @@
+<script lang="ts">
+	import Icon from "@iconify/svelte";
+	import { onMount } from "svelte";
+	import { page } from "$app/stores";
+	import { noryClient } from "$lib/nory";
+	import { supabaseClient } from "$lib/supabaseClient";
+
+	import UserProfile from "$lib/components/UserProfile.svelte";
+	import UserSetting from "$lib/components/UserSetting.svelte";
+
+	$: user = $page.data.user;
+	$: members = $page.data.members;
+	let selected = "class";
+</script>
+
+<main class="w-full flex flex-col">
+	<div class="p-4">
+		<UserProfile {user} />
+
+	</div>
+
+	<div class="p-4">
+		{#if user.name === user.username}
+			<div class="alert alert-warning">
+				anda belum merubah username
+			</div>
+		{/if}
+	</div>
+
+	<div class="tabs">
+		<btn
+			on:click={() => (selected = "class")}
+			class="tab tab-bordered"
+			class:tab-active={selected === "class"}
+		>
+			Kelas
+		</btn>
+		<btn
+			on:click={() => (selected = "setting")}
+			class="tab tab-bordered"
+			class:tab-active={selected === "setting"}
+		>
+			Pengaturan
+		</btn>
+	</div>
+
+	<section class="my-4 p-4">
+		{#if selected === "class"}
+			<div class="w-full flex flex-row justify-between">
+				<h2> Daftar Kelas: </h2>
+				<a href="{$page.url.href}/new-class" class="btn btn-primary btn-sm"> Buat Kelas </a>
+			</div>
+
+			<div class="py-4 grid grid-cols-1 gap-2">
+				{#each members as m (m.classId)}
+					<div class="card card-bordered bg-base-300 shadow">
+						<div class="card-body">
+							<span class="card-title">
+								{#await noryClient.getClassInfo(m.classId)}
+									{m.classId}
+								{:then { data: c }}
+									{c.name}
+								{:catch e}
+									{e.message}
+								{/await}
+							</span>
+							<div class="card-actions justify-end">
+								<a class="btn btn-primary btn-sm" href="/class/{m.classId}"> Kunjungi </a>
+							</div>
+						</div>
+					</div>
+				{/each}
+			</div>
+		{:else if selected === "setting"}
+			<UserSetting username={user.username} name={user.name} />
+		{/if}
+	</section>
+</main>
+<div class="flex-auto" />
---
+++ src/routes/(admin)/user/new-class/+page.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+	import { noryClient } from "$lib/nory";
+	import { goto } from "$app/navigation"
+
+	let name: string;
+	let description: string;
+	let loading = false;
+	$: disabled = loading || name === "" || description === "";
+
+	async function onSubmit() {
+		try {
+			loading = true;
+			await noryClient.createClass({ name, description });
+			goto("/user")
+		} finally {
+			loading = false;
+		}
+	}
+</script>
+
+<main>
+	<form class="form-control bg-base-200 gap-2 p-4" on:submit|preventDefault={onSubmit}>
+		<label class="input-group w-full">
+			<span> Name: </span>
+			<input bind:value={name} type="text" name="name" class="grow input input-bordered" />
+		</label>
+		<label class="input-group">
+			<span> Description: </span>
+			<input
+				bind:value={description}
+				type="text"
+				name="description"
+				class="grow input input-bordered"
+			/>
+		</label>
+
+		<button type="submit" class="btn btn-primary" class:loading {disabled}> Create </button>
+	</form>
+</main>
+
+<div class="flex-auto" />
