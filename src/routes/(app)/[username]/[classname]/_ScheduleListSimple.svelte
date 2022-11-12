111752de011ec20d9ebbb27c186936fd0d7038f3
falentio <riv.kevinfalent.io@gmail.com>
2022-11-12 12:59:26 +0700

    patch

--- src/routes/(app)/[username]/[classname]/_ScheduleListSimple.svelte
+++ src/routes/(app)/[username]/[classname]/_ScheduleListSimple.svelte
@@ -29,7 +29,6 @@
 					<div class="overflow-x-auto h-full">
 						{#each currentSchedules as s, i}
 							<div class="flex flex-row relative">
-								<span> {i + 1}. </span>
 								<span> {s.name} </span>
 								<span class="grow"></span>
 								<span class="sticky right-0 bg-base-300 pl-2"> {s.startAt.slice(11, -4)} </span>
