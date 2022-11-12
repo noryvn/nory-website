7a62fe0b3c70eda5067738f72fc89681302abaec
falentio <riv.kevinfalent.io@gmail.com>
2022-11-12 12:58:36 +0700

    patch

--- src/routes/(app)/[username]/[classname]/_ScheduleListSimple.svelte
+++ src/routes/(app)/[username]/[classname]/_ScheduleListSimple.svelte
@@ -20,26 +20,24 @@
 
 <div class="grid grid-cols-2 gap-2 max-w-md xl:max-w-lg mx-auto w-full">
 	{#each schedulesByDay as currentSchedules, i}
-		<div 
-			class="card card-compact bg-base-300 text-base-content min-h-[12rem]"
-		>
-			<div class="card-body">
-				<h3 class="text-xl"> {new Date(2005, 11, i + 4).toLocaleString(undefined, { weekday: "long" })} </h3>
-				<div class="overflow-x-auto h-full">
-					{#each currentSchedules as s, i}
-						<div class="flex flex-row relative">
-							<span> {i + 1}. </span>
-							<span> {s.name} </span>
-							<span class="grow"></span>
-							<span class="sticky right-0 bg-base-300 pl-2"> {s.startAt.slice(11, -4)} </span>
-						</div>
-					{:else}
-						<div class="text-center">
-							???
-						</div>
-					{/each}
+		{#if currentSchedules.length}
+			<div 
+				class="card card-compact bg-base-300 text-base-content min-h-[12rem]"
+			>
+				<div class="card-body">
+					<h3 class="text-xl"> {new Date(2005, 11, i + 4).toLocaleString(undefined, { weekday: "long" })} </h3>
+					<div class="overflow-x-auto h-full">
+						{#each currentSchedules as s, i}
+							<div class="flex flex-row relative">
+								<span> {i + 1}. </span>
+								<span> {s.name} </span>
+								<span class="grow"></span>
+								<span class="sticky right-0 bg-base-300 pl-2"> {s.startAt.slice(11, -4)} </span>
+							</div>
+						{/each}
+					</div>
 				</div>
 			</div>
-		</div>
+		{/if}
 	{/each}
 </div>
