54c9b9adafee7fe6fabd0448422ca167eab3934c
falentio <riv.kevinfalent.io@gmail.com>
2022-12-10 17:56:54 +0700

    chore: change login with google button

---
+++ src/lib/components/ThirdPartyLoginButton.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import Icon from "@iconify/svelte"
+
+	export let icon = ""
+</script>
+
+<button
+	type="button"
+	class="{$$props.class} select-none btn btn-wide btn-primary btn-outline w-full space-x-2 items-center"
+	on:click
+>
+	<Icon {icon} class="h-6 w-6" />
+	<span>
+		<slot />
+	</span>
+</button>
